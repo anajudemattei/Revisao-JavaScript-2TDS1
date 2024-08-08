b49d11a440cf9c0748054aeb0ae7a2e28206f938
Ana Julia Demattei <anajuliademattei26@gmail.com>
2024-08-08 11:39:16 -0300

    atividade 07

--- script07.js
+++ script07.js
@@ -1,5 +1,13 @@
 let metaLivros = 6;
 
-let livrosLidos = ["Para Todos Garotos que já amei", "Textos Crueis Demais Para Serem Lidos Rapidamente", "Antes que Tudo Acabe", "Todo esse Tempo"];
+let livrosLidos = ["Textos Crueis Demais Para Serem Lidos Rapidamente", "Para Todos Garotos que já Amei", "Antes que Tudo Acabe", "Harry Potter"];
 
-for (let livrosLidos)
+console.log("Seus livros lidos");
+    for (let i = 0; i <livrosLidos.length; i++){
+        console.log(`- ${livrosLidos[i]}`);
+    }
+
+    if (livrosLidos.length >= metaLivros){
+        console.log("Parabéns! Você atingiu sua meta de leitura!");
+    }else { console.log (`Você está progredindo! Faltam ${metaLivros - livrosLidos.length} livros para atingir sua meta.`);
+}
