70cb0b945ae90d556de8b7abdf688d01c0b1fb55
Ana Julia Demattei <anajuliademattei26@gmail.com>
2024-08-08 11:00:18 -0300

    atividade 06

--- script06.js
+++ script06.js
@@ -1,9 +1,17 @@
-let armas = ["laser", "misseis", "arma de fogo", "super soco", "lamina", "escudo"];
+let armas = ["Nome: Wall-E", "laser", "misseis", "arma de fogo", "super soco", "lamina", "escudo"];
 
-let escolhidos = [];
+let ataques = [];
 
-escolhidos.push(armas[0]);
-escolhidos.push(armas[6]);
-escolhidos.push(armas[3]);
+ataques.push("Wall-E");
+ataques.push(armas[5]);
+ataques.push(armas[3]);
+ataques.push(armas[4]);
 
+for (let i = 0; i < ataques.length; i++) {
+    if (i == 0) { 
+        console.log(`Nome do Robô: ${ataques[i]}`);
+    } else {
+        console.log(`Ataques: ${i}: ${ataques[i]}`);
+    }
+}
 
